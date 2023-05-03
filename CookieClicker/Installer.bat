a414d7b1197abd4cee1b6e12b2c75ab01a87d42a
GalaxyDoge72 <119555274+GalaxyDoge72@users.noreply.github.com>
2023-05-04 08:55:13 +1000

    2nd Release
    Added Grandma button (not working), cookie total text does not flicker

--- CookieClicker/Installer.bat
+++ CookieClicker/Installer.bat
@@ -2,4 +2,4 @@ python -m pip install --upgrade pip
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U
 python -m pip install pygame
 cd %~dp0
-echo main.py | cmd
+echo ReleaseVersion.py | cmd
